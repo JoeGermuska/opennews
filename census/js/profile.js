10daa6e85fd7eaaae7845085248bac871345b801
Gabriel Florit <gabriel.florit@velir.com>
2012-06-16 20:29:47 -0400

    added basic chart

--- census/js/profile.js
+++ census/js/profile.js
@@ -8,7 +8,7 @@ $(function(){
 
     function show_gender_breakdown(data) {
         breakdown = age_breakdown(data, 'PCT012', '2010');
-        
+        render_age_chart(breakdown);
     }
 
     function make_padded_key(table,fieldnum) {
@@ -24,16 +24,25 @@ $(function(){
         // male = PCT012003..102
         // female = PCT12107..206
         breakdown = {'male': [], 'female': [], 'total': [] };
+        breakdown.place = data.metadata.NAME;
         i = 0;
         for (var i = 0; i < 100; i++ ){
-            breakdown['male'][i] = ire_census.sf1val(data,make_padded_key(group,i+3),year)
-            breakdown['female'][i] = ire_census.sf1val(data,make_padded_key(group,i+107),year)
+            breakdown['male'][i] = Number(ire_census.sf1val(data,make_padded_key(group,i+3),year))
+            breakdown['female'][i] = Number(ire_census.sf1val(data,make_padded_key(group,i+107),year))
             breakdown['total'][i] = parseInt(breakdown['male'][i]) + parseInt(breakdown['female'][i])
+
         }
         return breakdown;
     }
 
-    function render_age_chart(title, breakdown) {
+    // breakdown will be an array of objects
+    // each object has female, male, total, place
+    function render_age_chart(breakdown) {
+
+        var series = [
+            {name:'female', data:breakdown.female},
+            {name:'male', data:breakdown.male}];
+
         chart = new Highcharts.Chart({
             chart: {
                 renderTo: 'chart',
@@ -41,21 +50,30 @@ $(function(){
                 marginRight: 130,
                 marginBottom: 25
             },
+            plotOptions: {
+                        line: {
+                            dataLabels: {
+                                // enabled: true
+                            },
+                            shadow: false,
+                            lineWidth: 3,
+                            marker: {
+                                enabled: false,
+                                radius: 3,
+                                lineWidth: 0,
+                                symbol: 'circle'
+                            }
+                        }
+                    },
             title: {
-                text: 'Monthly Average Temperature',
-                x: -20 //center
+                text: 'Population by Gender'
             },
             subtitle: {
-                text: 'Source: WorldClimate.com',
-                x: -20
-            },
-            xAxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-                    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+                text: ''
             },
             yAxis: {
                 title: {
-                    text: 'Temperature (Â°C)'
+                    text: ''
                 },
                 plotLines: [{
                     value: 0,
@@ -64,33 +82,14 @@ $(function(){
                 }]
             },
             tooltip: {
-                formatter: function() {
-                        return '<b>'+ this.series.name +'</b><br/>'+
-                        this.x +': '+ this.y +'Â°C';
-                }
             },
             legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'top',
-                x: -10,
-                y: 100,
-                borderWidth: 0
             },
-            series: [{
-                name: 'Tokyo',
-                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-            }, {
-                name: 'New York',
-                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-            }, {
-                name: 'Berlin',
-                data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-            }, {
-                name: 'London',
-                data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-            }]
+            series: series
         });
+
+
+
     }
 
     window.age_breakdown = age_breakdown;
